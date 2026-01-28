275445472d8ff68892956fd89c16ebd9324a1d72
Angoula Onambele Germain Raphael <farenogif05@gmail.com>
2026-01-28 16:26:33 +0100

    Commit 5

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Nav } from './nav/nav';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Nav],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
